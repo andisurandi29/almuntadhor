8fde0f42f580a1c7b6d1893b41c5efb6b9830abf
Arul Rachman Faruqhy <arulrachmanfaruqhy@gmail.com>
2022-06-11 09:46:56 +0700

    new page informasi

--- resources/views/pages/info.blade.php
+++ resources/views/pages/info.blade.php
@@ -25,11 +25,11 @@
                 @foreach($tampilContent as $gallery)
                 <div class="card-body bg-gradient-light" style="margin-bottom: 20px">
                   <h5>{{ $gallery->judul }}</h5>
-                  <span ><small >20/01/2022</small></span>
+                  <span ><small >Terbit : {{ $gallery->created_at }}</small></span>
                   <img src="{{ URL::to('/')}}/content/{{ $gallery->gambar }}" alt="" width="100%" style="margin-bottom: 10px; margin-top:10px">
                   <p>{{ $gallery->deskripsi }}</p>
                   <div class="text-center">
-                    <button class="btn btn-light">Selengkapnya...</button>
+                    <button href="#" class="btn btn-light">Selengkapnya...</button>
                   </div>
                 </div>
                 @endforeach
@@ -41,9 +41,9 @@
               <div class="card-body">
                 <h3>Arsip Informasi</h3>
                 <div class="list-group" style="margin-bottom: 50px">
-                  <a href="#" class="list-group-item list-group-item-action"> Judul 1</a>
-                  <a href="#" class="list-group-item list-group-item-action">Judul 2</a>
-                  <a href="#" class="list-group-item list-group-item-action">Judul 3</a>
+                  @foreach($tampilContent as $gallery)
+                  <a href="#" class="list-group-item list-group-item-action"> {{ $gallery->judul }}</a>
+                  @endforeach
                 </div>
                 <h3>Media Sosial Resmi</h3>
                 <p><small><i class="fab fa-instagram"></i> instagram : @pesantren_almuntadhor</small></p>
@@ -51,7 +51,7 @@
                 <p><small><i class="fab fa-youtube"></i> Youtube : almuntadhor.id</small></p>
                 <br>
                 <h3>Alamat</h3>
-             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto mollitia itaque laudantium tempore </p>
+             <p>Jl. Merdeka No. 61 RT. 02 RW. 03 Desa Babakan Kecamatan Ciwaringin Kabupaten Cirebon 45167</p>
              <iframe class="responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.658509229347!2d108.36614261404884!3d-6.68915276725453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6edefcaad37c35%3A0x163a0af6459df031!2sPondok%20Pesantren%20Al-muntadhor!5e0!3m2!1sid!2sid!4v1643802793550!5m2!1sid!2sid" width="250" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
             </div>
